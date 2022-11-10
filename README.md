6ff450823d60325c364c2475ae67c48923e6b9d3
Elisha Wong <elishawong@Elishas-MacBook-Pro.local>
2022-11-10 16:46:09 +0800

    added tailwind

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started
+A template for React Typescript project created with `npm create vite@latest`.
+
+Bundles with:
+
+1. Tailwind
+2. Eslint Airbnb for Typescript
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center h-[100vh]">
+      <div className="flex flex-col items-center justify-center mt-10 h-full gap-10">
+        <div className="text-4xl">Homepage</div>
+      </div>
+    </div>
+  );
+}
