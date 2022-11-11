aac37c54eb77dce71d965dc6ceb008ca2abbb936
Elisha Wong <elishawong@Elishas-MacBook-Pro.local>
2022-11-11 11:13:17 +0800

    update Layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Master from "../Layout/Master";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-[100vh]">
-      <div className="flex flex-col items-center justify-center mt-10 h-full gap-10">
-        <div className="text-4xl">Homepage</div>
-      </div>
-    </div>
+    <Master>
+      <div className="text-black">Content</div>
+    </Master>
   );
 }
